6ba61806e5abe3161b04a2e2efc77211abc695c2
Filip Stenbeck <filip.stenbeck@gmail.com>
2013-04-02 10:35:06 +0200

    added edit user frontend functionality

--- frontend/js/views.js
+++ frontend/js/views.js
@@ -7,40 +7,33 @@ app.UserView = Backbone.View.extend({
     },
     el: $("#app_container"),
     render: function () {
-        
-       $('#user-table tbody').empty();    
-        
+        $('#user-table tbody').empty();
         var that = this;
         _.each(this.coll.models, function (obj) {
-            $('#user-table tbody').append(_.template($("#all_user_template").html(), 
+            $('#user-table tbody').append(_.template($("#all_user_template").html(),
                 {
-                   'id' : obj.get('id'), 
-                    'name' : obj.get("name"), 
+                    'id' : obj.get('id'),
+                    'name' : obj.get("name"),
                     'isActive' : obj.get("isActive")
-                    }
-                ));    
+                }));  
             });
-            return this;
-            
-        },
+        return this;    
+    },
     
     //methods
     loadAllUsers : function () {
-    	
         var that = this;
-        
-        this.coll.fetch({
-                        
+        this.coll.fetch({        
             success: function (data) {
                 console.log('DEBUG: Loaded ' + that.coll.models.length + ' users from server.');
                 that.render();
             },
             error : function (error) {
-                console.log(error)         
+                console.log(error);         
             }
         });
     },
-    loaded : function (collection) {           
+    loaded : function (collection) {       
     },
     
     newUser : function () {
@@ -54,7 +47,7 @@ app.UserView = Backbone.View.extend({
 		}
         var newUser = new app.User();
         newUser.set({id : "NEW", name : newName, isActive : newIsActive});
-        newUser.save({},{               
+        newUser.save({}, {            
             success: function (data) {
 				newUser.set("id", data.id);  
 				app.userView.coll.add(newUser);
@@ -81,34 +74,59 @@ app.UserView = Backbone.View.extend({
         });
         
 		user.destroy({               
-            success: function (data) {  
+            success: function (data) {
 				app.userView.coll.remove(data);
                 app.userView.render();
             },
             error : function (e) {
-                console.log(e);             
+                console.log(e);            
             }
 		});
 		
 	},
 	 editUser : function (event) {
 		var id = event.currentTarget.id;
-		id = id.charAt(id.length-1);
+		id = id.charAt(id.length - 1);
 
-		var user = _.find(app.userView.coll.models, function(obj) {
+		var user = _.find(app.userView.coll.models, function (obj) {
 			if (obj.id == id) {
                 return obj;
             }
         });
+        $('#editIdField').html(user.get("id"));
         $('#editNameField').val(user.get("name"));
         $('#editIsActive').attr('checked', user.get("isActive"));
 		
 	},
-    
+    saveUser : function (event) {
+        var newName, newId, newIsActive, newUser;
+        
+        newName = $('#editNameField').val();
+        newId =   $('#editIdField').html();
+        newIsActive;
+        
+        if ($('#editIsActive:checked').val()) {
+			newIsActive = true;
+		} else {
+			newIsActive = false;
+		}
+        newUser = _.find(app.userView.coll.models, function(obj) {
+            return obj.get("id") == newId;
+        });
+        newUser.set({name : newName, isActive : newIsActive});
+        newUser.save({}, {
+            success: function (data) {
+                app.userView.render();
+            },
+            error : function (e) {
+                console.log(e);
+            }
+		});
+    },
     events: {
-         "click #newUserBtn" : "newUser",
-         "click #reloadBtn" : "loadAllUsers",
-         "click .delete-link" : "deleteUser",
-         "click .edit-link" : "editUser"
-  }
+        "click #newUserBtn" : "newUser",
+        "click #reloadBtn" : "loadAllUsers",
+        "click .delete-link" : "deleteUser",
+        "click .edit-link" : "editUser"
+    }
 });
